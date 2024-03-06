81733b07c6b81d99d09102db1a949ab48185565c
Ryan Quinn <ryanquinn@Ryans-Mac-mini.local>
2024-03-06 13:12:06 -0800

    re up 001

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -8,9 +8,6 @@ const HomePage = () => {
 
       <h1>HomePage</h1>
       <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
-      </p>
-      <p>
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
       </p>
